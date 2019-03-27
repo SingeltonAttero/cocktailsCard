d3776a3bdb03de749125cc9e6de1fb5153f3c815
YWeber <cen7ur1on007@gmail.com>
2019-03-28 00:58:24 +0200

    change styles ripple and icon tested

--- presentation/src/main/java/com/yakow/weber/cocktailsard/ui/cocktail/CocktailsFragment.kt
+++ presentation/src/main/java/com/yakow/weber/cocktailsard/ui/cocktail/CocktailsFragment.kt
@@ -20,16 +20,21 @@ class CocktailsFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         toolbar?.title = getString(R.string.cocktail)
+        mockData()
+
+    }
+
+    private fun mockData() {
         val mutableListOf = mutableListOf<Cocktail>()
-        for (i in 0..100){
-            val cocktail = Cocktail("Клубничный милкшейк ",
+        for (i in 0..100) {
+            val name = if (i % 2 == 1)  "Самый клубничный милкшейк" else "Клубничный милкшейк"
+            val cocktail = Cocktail(name,
                     "Клубника, молоко,  лед, земляника ",
                     "10 - 15 мин", "", 0,
-                    favorites = false, tested = false)
+                    favorites = i % 3 == 0, tested = i % 2 == 0)
             mutableListOf.add(cocktail)
         }
         cocktailsRecycler.adapter = CocktailsAdapter(mutableListOf)
         cocktailsRecycler.layoutManager = LinearLayoutManager(activity)
-
     }
 }
--- presentation/src/main/java/com/yakow/weber/cocktailsard/ui/cocktail/adapter/CocktailsAdapter.kt
+++ presentation/src/main/java/com/yakow/weber/cocktailsard/ui/cocktail/adapter/CocktailsAdapter.kt
@@ -3,9 +3,11 @@ package com.yakow.weber.cocktailsard.ui.cocktail.adapter
 import android.view.View
 import com.bumptech.glide.Glide
 import com.yakow.weber.cocktailsard.R
+import com.yakow.weber.cocktailsard.extension.visible
 import com.yakow.weber.cocktailsard.ui.base.adapter.BaseRecyclerAdapter
 import com.yakow.weber.cocktailsard.ui.base.adapter.BaseViewHolder
 import com.yakow.weber.domain.entity.Cocktail
+import kotlinx.android.synthetic.main.include_item_status.view.*
 import kotlinx.android.synthetic.main.item_cocktails_adapter.view.*
 
 /**
@@ -17,8 +19,6 @@ class CocktailsAdapter(override var dataSet: MutableList<Cocktail>) : BaseRecycl
 
     override fun getLayoutIdForPosition(position: Int): Int = R.layout.item_cocktails_adapter
 
-
-
     abstract class BaseCocktailsViewHolder(itemView: View) : BaseViewHolder<Cocktail>(itemView)
 
     inner class CocktailsViewHolder(itemView: View) : BaseCocktailsViewHolder(itemView) {
@@ -26,10 +26,11 @@ class CocktailsAdapter(override var dataSet: MutableList<Cocktail>) : BaseRecycl
             Glide.with(itemView)
                     .load("https://cdn.liquor.com/wp-content/uploads/2017/11/14145228/bacardi-cocktail-720x720-recipe.jpg")
                     .into(itemView.pictureCocktailView)
-            itemView.pictureCocktailView // TODO Image url
             itemView.nameCocktailView.text = item.nameCocktail
             itemView.descriptionConsistView.text = item.consist
             itemView.timePreparing.text = item.timePreparing
+            itemView.favoriteView.visible = item.favorites
+            itemView.drinkCocktailView.visible = item.tested
         }
     }
     inner class FirsCocktailsViewHolder(itemView: View) : BaseCocktailsViewHolder(itemView) {
